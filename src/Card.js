d397595c5d0a2da215c982a2505b0e10cb3dde49
triastanto <rahmadhany.triastanto@gmail.com>
2020-07-05 00:28:58 +0700

    Using robots.js for dynamic Card

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
     // This will not work because it needs to be wrapped inside 'div'
     // <h1>RoboFriends</h1>
-    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src="https://robohash.org/test?200x200" alt="robots"/>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      {/* using template literals syntax ${props.id} */}
+      <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"/>
       <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe.gmail.com</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
