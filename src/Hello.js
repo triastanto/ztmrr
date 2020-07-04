f3d29dd795660512c3e347e5c26e956af13735cf
triastanto <rahmadhany.triastanto@gmail.com>
2020-07-05 00:02:38 +0700

    Replace Hello class into function

--- src/Hello.js
+++ src/Hello.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Hello.css';
 
-class Hello extends Component {
-  render() {
-    return (
-      <div className='f1 tc'>
-        <h1>Hello World</h1>
-        <p>{this.props.greeting}</p>
-      </div>
-    )
-  }
+const Hello = (props) => {
+  return (
+    <div className='f1 tc'>
+      <h1>Hello World</h1>
+      <p>{props.greeting}</p>
+    </div>
+  )
 }
 
 export default Hello;
