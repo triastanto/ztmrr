13423a3359b93240419a7292e8bf01eec57ddf93
triastanto <rahmadhany.triastanto@gmail.com>
2020-07-05 15:51:24 +0700

    Clean comments

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
-// instead of using 'props' only and accessing by 'props.*'
-// const Card = (props) => {
-// do destructuring in function parameter
 const Card = ({ id, name, email }) => {
   return (
-    // This will not work because it needs to be wrapped inside 'div'
-    // <h1>RoboFriends</h1>
+
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      {/* using template literals syntax ${id} */}
       <img src={`https://robohash.org/${id}?200x200`} alt="robots"/>
       <div>
         <h2>{name}</h2>
--- src/CardList.js
+++ src/CardList.js
@@ -2,17 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  // using variables method
-  // const cards = robots.map((user, i) => {
-  //   return (<Card key={i}
-  //     id={robots[i].id}
-  //     name={robots[i].name}
-  //     email={robots[i].email}
-  //   />);
-  // });
-  // return <div> {cards} </div>;
-
-  // using parameter directly
   return (
     <div>
       {
