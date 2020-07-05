47793127fb96bfc806a51c6a1e38e719e7e5d789
triastanto <rahmadhany.triastanto@gmail.com>
2020-07-05 15:59:33 +0700

    Implement onChange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots.js';
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className='tc' >
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
 }
 
 export default App;
