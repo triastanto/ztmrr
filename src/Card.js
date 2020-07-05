4f427271daaaa79a65cbc77bc6058f60fe4917d5
triastanto <rahmadhany.triastanto@gmail.com>
2020-07-05 14:42:12 +0700

    Destructuring props in parameter

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-const Card = (props) => {
+// instead of using 'props' only and accessing by 'props.*'
+// const Card = (props) => {
+// do destructuring in function parameter
+const Card = ({ id, name, email }) => {
   return (
     // This will not work because it needs to be wrapped inside 'div'
     // <h1>RoboFriends</h1>
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      {/* using template literals syntax ${props.id} */}
-      <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"/>
+      {/* using template literals syntax ${id} */}
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots"/>
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
