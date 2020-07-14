e810ca3221bf797c11c5b758f165ebac6e851681
triastanto <rahmadhany.triastanto@gmail.com>
2020-07-14 20:05:54 +0700

    using fetch for external API

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
       robots: [],
       searchField: ''
     }
-    console.log('constructor');
   }
 
   componentDidMount() {
-    this.setState({ robots: robots });
-    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => { return response.json() })
+      .then(users => { this.setState({ robots: users }) });
   }
 
   onSearchChange = (event) => {
@@ -27,14 +27,17 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
-    console.log('render');
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
