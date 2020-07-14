5b2c45c36e95a6f76b5328c345351ae4403133ec
triastanto <rahmadhany.triastanto@gmail.com>
2020-07-14 10:48:26 +0700

    styling sega logo font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots.js';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -21,8 +22,8 @@ class App extends Component {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
     return (
-      <div className='tc' >
-        <h1>RoboFriends</h1>
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
